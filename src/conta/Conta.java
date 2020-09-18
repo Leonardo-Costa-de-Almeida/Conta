497add77456ef0c3d118f993fb9f4d2460280b65
Leonardo-Costa-de-Almeida <62718629+Leonardo-Costa-de-Almeida@users.noreply.github.com>
2020-09-18 20:56:23 -0300

    num sei faze bibit

--- src/conta/Conta.java
+++ src/conta/Conta.java
@@ -1,30 +1,36 @@
 package conta;
 
-public class Conta {
+import java.util.Scanner;
 
+public class Conta {
+	Scanner sc = new Scanner(System.in);
 	String name;
 	int cdigitos;
 	int saldo;
-	
-	//constructor
-	public Conta(String name, int cdigitos, int saldo) 
-	    { 
-	        this.name = name; 
-	        this.cdigitos = cdigitos; 
-	        this.saldo = saldo; 
-	     
-	    }
-	
-	//methods
+
+	// constructor
+	public Conta(String name, int cdigitos, int saldo) {
+
+		this.name = name;
+		this.cdigitos = cdigitos;
+		this.saldo = saldo;
+
+	}
+
+	// methods
 	public String getName() {
+		name = sc.next();
+		
 		return name;
 	}
-	
+
 	public int getCdigitos() {
+		cdigitos = sc.nextInt();
 		return cdigitos;
 	}
-	
+
 	public int saldo() {
+		saldo = sc.nextInt();
 		return saldo;
 	}
 }
--- src/conta/Main.java
+++ src/conta/Main.java
@@ -5,13 +5,56 @@ import java.util.Scanner;
 
 public class Main {
 	
-	Scanner sc = new Scanner(System.in);
-
-	private String nome;
-	private int cdigito;
-	private int saldo;
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int escolha = 0;
+		String nome = " " ;
+		int cdigito = 0 ;
+		int saldo =  0;
+		
+		Conta contas = new Conta(nome, cdigito, saldo);
+		
+		System.out.println("Olá, bem vindo ao Banco bibitos!");
+		
+		//criando conta
+		System.out.println("Digite seu nome: ");
+		nome = contas.getName();
+		System.out.println("Muito bem!");
+		System.out.println("Agora digite o numero da conta: ");
+		cdigito = contas.getCdigitos();
+		System.out.println("Sua conta foi criada com sucesso!");
+		
+		//depois inicial
+		System.out.println("deseja realizar um deposito incial?");
+		System.out.println("Sim digite 1 / Não digite 2");
+		escolha = sc.nextInt();
+		if (escolha == 1) {
+			
+			System.out.println("seu saldo atual é: " + contas.saldo());
+		}
+		else if(escolha == 2) {
+			System.out.println("seu saldo atual é: " + contas.saldo());
+		}
+		
 	
-	Conta contas = new Conta(nome, cdigito, saldo);
-	System.out.println("1 Alcool / 2 Gasolina / 3 Disel");
+		System.out.println("deseja realizar uma transferencia? P/ saque digite 1 P/ deposito digite 2: ");
+		escolha =  sc.nextInt();
+		
+		if ( escolha == 1) {
+			if(contas.saldo() == 0) {
+				System.out.println("seu saldo atual é: " + contas.saldo() + " voce nao possui saldo para saque");
+			}
+			else{
+				//pedir valor do saque -3
+			}
+				
+		}else if(escolha == 2) {
+			System.out.println("digite o valor do deposito(");
+		}
+	}
+
+	
+	
 }
  
