3800045d47354756857a1d2b42e45dc74aff2216
Leonardo-Costa-de-Almeida <62718629+Leonardo-Costa-de-Almeida@users.noreply.github.com>
2020-09-18 19:58:50 -0300

    why

--- src/conta/Main.java
+++ src/conta/Main.java
@@ -2,8 +2,16 @@ package conta;
 
 import java.util.Scanner;
 
+
 public class Main {
-
 	
+	Scanner sc = new Scanner(System.in);
 
+	private String nome;
+	private int cdigito;
+	private int saldo;
+	
+	Conta contas = new Conta(nome, cdigito, saldo);
+	System.out.println("1 Alcool / 2 Gasolina / 3 Disel");
 }
+ 
