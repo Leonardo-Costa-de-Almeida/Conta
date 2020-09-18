860e4c5177036d6f26e9465ce384d2e5f72db0d6
Leonardo-Costa-de-Almeida <62718629+Leonardo-Costa-de-Almeida@users.noreply.github.com>
2020-09-18 16:25:40 -0300

    conta

---
+++ src/conta/Conta.java
@@ -0,0 +1,27 @@
+package conta;
+
+import java.util.Scanner;
+
+public class Conta {
+
+	public void dados(String a, int b, int c) {
+		String name;
+		int cdigitos;
+		int saldo = 5;
+	}
+	
+
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o nome");
+		String a;
+		int b;
+		int c;
+		
+		sc.next() = dados(a);
+		//depressao
+	}
+
+}
