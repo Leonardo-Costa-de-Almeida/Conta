e6a535a31ace2c9cb95c143eaec8b352036cc7ae
Leonardo-Costa-de-Almeida <62718629+Leonardo-Costa-de-Almeida@users.noreply.github.com>
2020-09-19 19:43:50 -0300

    pronto

--- src/conta/Main.java
+++ src/conta/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		int escolha = 0;
 		String nome = " ";
 		int cdigito = 0;
-		int saldo = 0;
+		double saldo = 0;
 
 		Conta contas = new Conta(nome, cdigito, saldo);
 
@@ -43,33 +43,51 @@ public class Main {
 		System.out.println("Sua conta foi criada com sucesso!");
 
 		// mostrando dados da conta
-		System.out.println("nome: " + contas.getName() + " Digitos: " + contas.getCdigitos() + " saldo: " + contas.saldo() );
+		System.out.println(
+				"nome: " + contas.getName() + " Digitos: " + contas.getCdigitos() + " saldo: " + contas.saldo());
 
-		System.out.println("qual operacao deseja realizar? P/ saque digite 1 P/ deposito digite 2 P/ trocar de nome  ");
+		System.out.println(
+				"qual operacao deseja realizar? ||P/ saque digite 1 ||P/ deposito digite 2|| P/ trocar de nome digite 3||  ");
 		escolha = sc.nextInt();
 
-		while(escolha != 0) {
-			
+		while (escolha != 0) {
+
 			if (escolha == 1) {
-				if (contas.saldo() == 0) {
-					System.out.println("seu saldo atual é: " + contas.saldo() + " voce nao possui saldo para saque");
-				} else {
-					// pedir valor do saque -3
-					//saque
-				}
+
+				System.out.println("Digite o valor do saque: ");
+
+				double newsaldo = sc.nextDouble();
+				saldo = contas.saldo();
+				saldo = saldo - newsaldo - 3;
+				contas.setSaldo(saldo);
+				System.out.println("seu novo saldo: " + contas.saldo());
 
 			} else if (escolha == 2) {
-				System.out.println("digite o valor do deposito(");
-				//deposito
-			}else if(escolha == 3) {
-				//troca nome
-			}else if(escolha == 0) {
-				System.out.println("obrigado por usar o banco do Bibito");
+				System.out.println("digite o valor do deposito: ");
+				double newsaldo = sc.nextDouble();
+				saldo = contas.saldo();
+				saldo = saldo + newsaldo;
+				contas.setSaldo(saldo);
+				System.out.println("seu novo saldo: " + contas.saldo());
+			} else if (escolha == 3) {
+				System.out.println("digite o seu novo nome: ");
+				String newname = sc.next();
+				nome = newname;
+				contas.setName(nome);
+				System.out.println("seu novo nome é: " + contas.getName());
+			}else if(escolha <= 0 || escolha > 3) {
+				System.out.println("digite uma escolha valida");
 			}
 			
+			System.out.println("qual operacao deseja realizar? ||P/ saque digite 1 ||P/ deposito digite 2|| P/ trocar de nome digite 3||  ");
+
+			escolha = sc.nextInt();
+
 		}
-		
-	sc.close();	
+
+		System.out.println("obrigado por usar o banco do Bibito");
+
+		sc.close();
 	}
 
 }
